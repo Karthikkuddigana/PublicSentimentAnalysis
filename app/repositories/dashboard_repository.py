1d454885d04627378f8e8c233136ee2f4760ed5f
Krishna Murali Andiboyina <142897461+krishna1330@users.noreply.github.com>
2026-02-21 21:28:08 +0000

    1 linw

--- app/repositories/dashboard_repository.py
+++ app/repositories/dashboard_repository.py
@@ -11,11 +11,17 @@ def get_sentiment_counts(organization_id: str):
         .execute()
     )
 
+    response2 = (supabase.table("manual_reviews").select("sentiment").eq("organization_id", organization_id).execute())
+
     rows = response.data
+    rows2 = response2.data
 
     positive = sum(1 for r in rows if r["sentiment"] == "Positive")
+    positive += sum(1 for r in rows2 if r["sentiment"] == "Positive")
     negative = sum(1 for r in rows if r["sentiment"] == "Negative")
+    negative = sum(1 for r in rows2 if r["sentiment"] == "Negative")
     neutral = sum(1 for r in rows if r["sentiment"] == "Neutral")
+    neutral += sum(1 for r in rows2 if r["sentiment"] == "Neutral")
 
     return {
         "positiveCount": positive,
@@ -34,11 +40,25 @@ def get_emotion_counts(organization_id: str):
         .execute()
     )
 
+    response2 = (
+        supabase
+        .table("manual_reviews")
+        .select("emotion")
+        .eq("organization_id", organization_id)
+        .execute()
+    )
+
     rows = response.data
+    rows2 = response.data
 
     emotion_counts = {}
 
     for r in rows:
+        emotion = r["emotion"]
+        if emotion:
+            emotion_counts[emotion] = emotion_counts.get(emotion, 0) + 1
+
+    for r in rows2   :
         emotion = r["emotion"]
         if emotion:
             emotion_counts[emotion] = emotion_counts.get(emotion, 0) + 1
